6e0ee4ee97accd523e68a552e855a6c5705be913
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-12 14:41:37 -0500

    Create coinflip.js

---
+++ commands/fun/coinflip.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports={
+
+data:new SlashCommandBuilder()
+.setName("coinflip")
+.setDescription("Flip a coin"),
+
+async execute(interaction){
+
+const result = Math.random() < 0.5 ? "Heads" : "Tails";
+
+interaction.reply(`Result: ${result}`);
+
+}
+
+};
