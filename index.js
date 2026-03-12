1cc35ca527e1c790024f48491475415db684504d
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-12 14:31:13 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const mongoose = require("mongoose");
+
+const loadCommands = require("./core/loader");
+
+const client = new Client({
+intents:[
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMembers
+]
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"));
+
+loadCommands(client);
+
+require("./events/interactionCreate")(client);
+require("./events/buttons")(client);
+require("./events/ready")(client);
+
+client.login(process.env.TOKEN);
