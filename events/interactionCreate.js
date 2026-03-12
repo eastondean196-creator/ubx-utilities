fd4c672c41e3670cd36e7b2282ba8821152922d0
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-12 14:33:54 -0500

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,23 @@
+module.exports = (client)=>{
+
+client.on("interactionCreate",async interaction=>{
+
+if(!interaction.isChatInputCommand()) return;
+
+const command = client.commands.get(interaction.commandName);
+
+if(!command) return;
+
+try{
+
+await command.execute(interaction,client);
+
+}catch(err){
+
+console.error(err);
+
+}
+
+});
+
+};
