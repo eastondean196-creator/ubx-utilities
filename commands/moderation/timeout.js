999032ed4a791f162a2b22bdcbe79f78f7b6f4e3
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-13 12:26:34 -0500

    Update timeout.js

--- commands/moderation/timeout.js
+++ commands/moderation/timeout.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { sendModLog } = require("../../core/modLogs");
 
 module.exports = {
 
@@ -13,16 +14,41 @@ o.setName("minutes").setDescription("Duration in minutes").setRequired(true))
 
 async execute(interaction){
 
+try{
+
 await interaction.deferReply();
 
 const user = interaction.options.getUser("user");
 const minutes = interaction.options.getInteger("minutes");
 
+if(minutes <= 0 || minutes > 10080){
+return interaction.editReply("Minutes must be between 1 and 10080.");
+}
+
 const member = await interaction.guild.members.fetch(user.id);
+
+if(!member.moderatable){
+return interaction.editReply("I cannot timeout this user.");
+}
 
 await member.timeout(minutes * 60 * 1000);
 
-interaction.editReply(`${user.tag} timed out for ${minutes} minutes`);
+await interaction.editReply(`${user.tag} timed out for ${minutes} minutes`);
+
+await sendModLog(interaction.client, interaction.guild, {
+title:"⏳ Timeout",
+color:"Yellow",
+user:user.tag,
+moderator:interaction.user.tag,
+reason:`${minutes} minutes`
+});
+
+}catch(error){
+
+console.error(error);
+interaction.editReply("Failed to timeout user.");
+
+}
 
 }
 
