e1c1effafadb341b29945298d41c128ccaafaa8a
RVEPRTY <n3xtt1m3off@gmail.com>
2026-03-13 11:09:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const mongoose = require("mongoose");
+
 const loadCommands = require("./core/loader");
 
 const client = new Client({
@@ -10,77 +15,6 @@ intents:[
 GatewayIntentBits.Guilds,
 GatewayIntentBits.GuildMembers
 ]
-});
-
-client.on("interactionCreate", async interaction => {
-
-if(!interaction.isButton()) return;
-
-const { EmbedBuilder } = require("discord.js");
-
-if(interaction.customId === "help_mod"){
-
-const embed = new EmbedBuilder()
-.setTitle("Moderation Commands")
-.setDescription(`
-/ban
-/kick
-/purge
-/slowmode
-/lock
-/unlock
-`)
-.setColor(0x5865F2);
-
-interaction.reply({embeds:[embed],ephemeral:true});
-
-}
-
-if(interaction.customId === "help_links"){
-
-const embed = new EmbedBuilder()
-.setTitle("Link System Commands")
-.setDescription(`
-/panel
-/addlink
-/bulkadd
-/removelink
-`)
-.setColor(0x57F287);
-
-interaction.reply({embeds:[embed],ephemeral:true});
-
-}
-
-if(interaction.customId === "help_utils"){
-
-const embed = new EmbedBuilder()
-.setTitle("Utility Commands")
-.setDescription(`
-/info
-/status
-/op
-/help
-`)
-.setColor(0xFEE75C);
-
-interaction.reply({embeds:[embed],ephemeral:true});
-
-}
-
-if(interaction.customId === "help_fun"){
-
-const embed = new EmbedBuilder()
-.setTitle("Fun Commands")
-.setDescription(`
-/coinflip
-`)
-.setColor(0xED4245);
-
-interaction.reply({embeds:[embed],ephemeral:true});
-
-}
-
 });
 
 mongoose.connect(process.env.MONGO_URI)
