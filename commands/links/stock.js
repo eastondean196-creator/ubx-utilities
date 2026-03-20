f177388f28d3d19103e5faaf9fdd095aff56c843
offensivea22-web <offensivea22@gmail.com>
2026-03-20 18:57:14 -0500

    Update stock.js

--- commands/links/stock.js
+++ commands/links/stock.js
@@ -10,19 +10,16 @@ data: new SlashCommandBuilder()
 
 async execute(interaction){
 
-// Get links
+
 const fullLinks = await Links.find({ type:"full", used:false });
 const liteLinks = await Links.find({ type:"lite", used:false });
 
-// Counts
 const fullCount = fullLinks.length;
 const liteCount = liteLinks.length;
 
-// Format lists
 const fullList = fullLinks.map(link => link.url).join("\n");
 const liteList = liteLinks.map(link => link.url).join("\n");
 
-// File content
 const textContent = 
 `NRG LINK STOCK EXPORT
 
@@ -41,16 +38,16 @@ Available: ${liteCount}
 ${liteList || "None"}
 `;
 
-// Convert to file
+
 const buffer = Buffer.from(textContent, "utf-8");
 
 const file = new AttachmentBuilder(buffer, {
 name: "nrg-link-stock.txt"
 });
 
-// Send file
+
 await interaction.reply({
-content: "📄 Here is the full NRG link stock export.",
+content: "Here is the full UBX link stock export.",
 files: [file],
 ephemeral: true
 });
