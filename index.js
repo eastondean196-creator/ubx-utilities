853faf82dda552642321bf92db3b1d42fe1799f1
offensivea22-web <offensivea22@gmail.com>
2026-03-22 14:29:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,5 @@ loadCommands(client);
 require("./events/interactionCreate")(client);
 require("./events/buttons")(client);
 require("./events/ready")(client);
-require("./events/guildMemberAdd")(client);
 
 client.login(process.env.TOKEN);
